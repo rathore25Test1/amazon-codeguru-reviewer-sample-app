a36be3a38f117d6a9f0ec64ae6aea2be48daac0d
rathore25Test1 <nitish.rathore25+test1@gmail.com>
2024-08-01 15:48:05 -0700

    Update Main.java

--- src/some-package/java/com/mainpackage/Main.java
+++ src/some-package/java/com/mainpackage/Main.java
@@ -16,7 +16,9 @@ public class Main {
     public static void main(String[] argv) {
         String message = argv[0];
         String key = argv[1];
-        Cipher cipher = Cipher.getInstance("RSA");
+        // Critical Finding
+        // Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance("AES");
         SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         System.out.println(String(cipher.doFinal(message.getBytes()), StandardCharsets.UTF_8));
